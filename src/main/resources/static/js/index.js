414e55e39fadee2d4289262a4eb7fd4641f803b3
Jiří Zeman <jirka.zemann@gmail.com>
2023-12-16 13:27:33 +0100

    add last searched feature add table info about M+ score into modal

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -10,13 +10,13 @@ const drawTable = (characterData) => {
     data.addColumn('string', 'Character Name');
     data.addColumn('string', 'Realm');
     data.addColumn('string', 'TOP 8 Dungeons');
-    data.addColumn('number', 'T1');
-    data.addColumn('number', 'T4');
-    data.addColumn('number', 'T8');
+    data.addColumn('number', 'Vault 1');
+    data.addColumn('number', 'Vault 4');
+    data.addColumn('number', 'Vault 8');
     data.addColumn('number', 'Total');
 
     if (characterData.length > 0) {
-        characterData.forEach(({name, realm, dungeons, tierOneScore,tierFourScore,tierEightScore,totalScore}) => {
+        characterData.forEach(({name, realm, dungeons, tierOneScore, tierFourScore, tierEightScore, totalScore}) => {
             let finishedDungeons = undefined;
             if (dungeons.length > 0) {
                 finishedDungeons = dungeons.map(({shortName, finishedKeyLevel}) => {
@@ -24,7 +24,7 @@ const drawTable = (characterData) => {
                 }).join(", ");
             }
             data.addRows([
-                [name, realm, finishedDungeons, tierOneScore,tierFourScore,tierEightScore,totalScore]
+                [name, realm, finishedDungeons, tierOneScore, tierFourScore, tierEightScore, totalScore]
             ]);
         });
     }
@@ -63,38 +63,79 @@ const hideLoader = () => {
     loaderContainer.style.display = 'none';
 }
 
-const formSubmitHandler = () => {
-    const form = document.getElementById('characterForm');
+const setLocalStorageData = (localStorageName, jsonObject) => {
+    const jsonString = JSON.stringify(jsonObject);
+    localStorage.setItem(localStorageName, jsonString);
+}
+
+const getLocalStorageData = (localStorageName) => {
+    const storedData = localStorage.getItem(localStorageName);
+    return JSON.parse(storedData);
+}
+
+const setJsonObjectForLocalStorage = (name) => {
+    const array = name.split(' ');
+
+    const arrayOfNames = array.map((value) => ({"name": value}));
+    setLocalStorageData('lastSearched', arrayOfNames)
+}
+
+const getJsonObjectForLocalStorage = (localStorageName) => {
+    const lastSearchedData = getLocalStorageData(localStorageName);
+
+    return lastSearchedData.map(({name}) => name).join(' ');
+}
+
+const fetchData = (name) => {
     const submitButton = document.getElementById('submit');
-    const nameInput = document.getElementById('characterName');
     const prevWeekCheckbox = document.getElementById('prevWeek');
     const dummyModeCheckbox = document.getElementById('dummyMode');
+    const currentWeek = !prevWeekCheckbox.checked;
+    const dummyMode = dummyModeCheckbox.checked;
+
+    submitButton.disabled = true;
+    showLoader();
+    fetch(`/characters?name=${name}&currentWeek=${currentWeek}&dummyMode=${dummyMode}`)
+        .then(response => response.json())
+        .then((characterData) => {
+            hideLoader();
+            modal();
+            drawTable(characterData);
+            submitButton.disabled = false;
+        })
+        .catch(error => {
+            hideLoader();
+            submitButton.disabled = false;
+            console.error('Error fetching character data:', error);
+        });
+}
+
+const formSubmitHandler = () => {
+    const form = document.getElementById('characterForm');
+    const nameInput = document.getElementById('characterName');
 
     form.addEventListener('submit', (event) => {
         event.preventDefault();
         const name = nameInput.value;
-        const currentWeek = !prevWeekCheckbox.checked;
-        const dummyMode = dummyModeCheckbox.checked;
-        submitButton.disabled = true;
-        showLoader();
 
-        fetch(`/characters?name=${name}&currentWeek=${currentWeek}&dummyMode=${dummyMode}`)
-            .then(response => response.json())
-            .then((characterData) => {
-                hideLoader();
-                modal();
-                drawTable(characterData);
-                submitButton.disabled = false;
-            })
-            .catch(error => {
-                hideLoader();
-                submitButton.disabled = false;
-                console.error('Error fetching character data:', error);
-            });
+        setJsonObjectForLocalStorage(name)
+        fetchData(name)
+    });
+}
+
+const lastSearchedHandle = () => {
+    const lastSearchedButton = document.getElementById('lastSearched');
+
+    lastSearchedButton.addEventListener('click', (event) => {
+        event.preventDefault();
+        const lastSearchedData = getJsonObjectForLocalStorage('lastSearched');
+
+        fetchData(lastSearchedData);
     });
 }
 
 document.addEventListener('DOMContentLoaded', () => {
+    lastSearchedHandle();
     formSubmitHandler();
 
     google.charts.load('current', {
